902941b5a610c794e8b1684fa0f00d23200b5c78
Matheus Viscki <matheusviscki@gmail.com>
2025-01-08 20:49:41 -0300

    feat: criar componente exibir usuarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { UserComponent } from "./components/user/user.component";
+import { DUMMY_USERS } from './dummy.users';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,9 @@ import { UserComponent } from "./components/user/user.component";
 })
 export class AppComponent {
   title = 'angular-app';
+  users = DUMMY_USERS;
+
+  onUserClick() {
+    console.log('Avatar clicked');
+  }
 }
--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, EventEmitter, Input, Output, input } from '@angular/core';
+import { DUMMY_USERS } from '../../dummy.users';
 
 @Component({
   selector: 'app-user',
@@ -7,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input({ required: true }) avatar!: string;
+  @Input() name!: string;
+  @Output() userClick = new EventEmitter<string>();
 
+  get imagePath() {
+    return `users/${this.avatar}`;
+  }
+
+  // usando signals
+  avatarSignal = input<string>();
+  nameSignal = input<string>();
+  imagePathSignal = computed(() => `users/${this.avatarSignal()}`);
+
+  onUserClick() {
+    this.userClick.emit();
+  }
 }
