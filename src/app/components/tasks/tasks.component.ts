93f8822e316a56b14cbc6e50b4f976a7180a89c4
Matheus Viscki <matheusviscki@gmail.com>
2025-01-09 22:16:10 -0300

    feat: implementar componente de adição de tarefa

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,16 +1,19 @@
 import { Component, input, Input } from '@angular/core';
 import { TaskComponent } from "../task/task.component";
 import { DUMMY_TASKS } from '../../dummy.tasks';
+import { NewTaskComponent } from "../new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
   @Input({ required: true }) name!: string;
   @Input({ required: true }) userId!: string;
+
+  public isAddingTask = false;
   private tasks = DUMMY_TASKS;
 
   get userTasks() {
@@ -19,5 +22,13 @@ export class TasksComponent {
 
   onCompleteTask(taskId: string) {
     this.tasks = this.tasks.filter(task => task.id !== taskId);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = !this.isAddingTask;
+  }
+
+  onCancelTask() {
+    this.isAddingTask = !this.isAddingTask;
   }
 }
