acb1a79e3532de5ae56dc7ecf4726cc29202bc71
Matheus Viscki <matheusviscki@gmail.com>
2025-01-14 06:52:32 -0300

    feat: adicionar salvamento em localStorage

---
+++ src/app/local-storage/index.ts
@@ -0,0 +1,6 @@
+const APP_PREFIX = '@angular-app'; // Boa pratica para evitar conflitos com outras aplicações
+const STORAGE_KEY = `${APP_PREFIX}-tasks`;
+
+export {
+    STORAGE_KEY
+}
--- src/app/services/task/task.service.ts
+++ src/app/services/task/task.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
-import { DUMMY_TASKS } from '../../dummy.tasks';
+import { ITask } from '../../dummy.tasks';
 import { INewTask } from '../../components/new-task/new-task.component';
+import { STORAGE_KEY } from '../../local-storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-  private tasks = DUMMY_TASKS;
+  private tasks: ITask[] = [];
+
+  constructor() {
+    const dataLocalStorage = localStorage.getItem(STORAGE_KEY);
+
+    this.tasks = dataLocalStorage ? JSON.parse(dataLocalStorage) : [];
+   }
 
   userTasks(userId: string) {
     return this.tasks.filter(task => task.userId === userId);
@@ -14,15 +21,23 @@ export class TaskService {
 
   completeTask(taskId: string) {
     this.tasks = this.tasks.filter(task => task.id !== taskId);
+    this.saveTasks();
   }
 
     addTask(newTask: INewTask, userId: string) {
-      this.tasks.push({
+      const addTask = {
         id: new Date().getTime().toString(),
         userId: userId,
         title: newTask.title,
         summary: newTask.summary,
         dueDate: newTask.dueDate
-      })
+      }
+
+      this.tasks.push(addTask);
+      this.saveTasks();
+    }
+
+    private saveTasks() {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(this.tasks));
     }
 }
